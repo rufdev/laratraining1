250724227a3dfe17b1d13e1643bb89134358514d
RUF.DEV <aguilarufino@gmail.com>
2025-09-23 08:59:50 +0800

    added asignment

---
+++ resources/js/pages/Asset/Index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+    import AppLayout from '@/layouts/AppLayout.vue';
+</script>
+<template>
+    <AppLayout>
+        <h1>Asset Page</h1>
+    </AppLayout>
+</template>
---
+++ resources/js/pages/Location/Index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+    import AppLayout from '@/layouts/AppLayout.vue';
+</script>
+<template>
+    <AppLayout>
+        <h1>Location Page</h1>
+    </AppLayout>
+</template>
