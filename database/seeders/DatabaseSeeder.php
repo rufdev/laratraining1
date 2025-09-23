aa804ca3d0c47659cb68d53c685fa23fad1360a5
RUF.DEV <aguilarufino@gmail.com>
2025-09-23 14:09:26 +0800

    added master file and asset seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Manufacturer;
+use App\Models\Category;
+use App\Models\Location;
+use App\Models\Asset;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +18,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
  
-        $this->call(UserSeeder::class);
+        // $this->call(UserSeeder::class);
+
+        User::factory(20)->create();
+        // Create 10 more random manufacturers
+        Manufacturer::factory(10)->create();
+
+        // Create 10 more random categories
+        Category::factory(10)->create();
+
+        // Create 10 more random locations
+        Location::factory(10)->create();
+
+        // Create 100 random assets
+        // These assets will randomly pick existing categories, locations, manufacturers, and users
+        // (including the specific ones from the UserSeeder and the random ones from UserFactory)
+        Asset::factory(100)->create();
     }
 }
