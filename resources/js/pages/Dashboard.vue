8f7a551b44e07bf9e68cab2f19320b0f88fb7bd6
RUF.DEV <aguilarufino@gmail.com>
2025-09-24 15:43:35 +0800

    added ReusableDataTable

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,4 +1,22 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+    Select,
+    SelectContent,
+    SelectItem, 
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
@@ -17,7 +35,9 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Dashboard" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+        <div
+            class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
+        >
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div
                     class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border"
@@ -38,7 +58,54 @@ const breadcrumbs: BreadcrumbItem[] = [
             <div
                 class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border"
             >
-                <PlaceholderPattern />
+                <Card class="w-[350px]">
+                    <CardHeader>
+                        <CardTitle>Create project</CardTitle>
+                        <CardDescription
+                            >Deploy your new project in
+                            one-click.</CardDescription
+                        >
+                    </CardHeader>
+                    <CardContent>
+                        <form>
+                            <div class="grid w-full items-center gap-4">
+                                <div class="flex flex-col space-y-1.5">
+                                    <Label for="name">Name</Label>
+                                    <Input
+                                        id="name"
+                                        placeholder="Name of your project"
+                                    />
+                                </div>
+                                <div class="flex flex-col space-y-1.5">
+                                    <Label for="framework">Framework</Label>
+                                    <Select>
+                                        <SelectTrigger id="framework">
+                                            <SelectValue placeholder="Select" />
+                                        </SelectTrigger>
+                                        <SelectContent position="popper">
+                                            <SelectItem value="nuxt">
+                                                Nuxt
+                                            </SelectItem>
+                                            <SelectItem value="next">
+                                                Next.js
+                                            </SelectItem>
+                                            <SelectItem value="sveltekit">
+                                                SvelteKit
+                                            </SelectItem>
+                                            <SelectItem value="astro">
+                                                Astro
+                                            </SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+                            </div>
+                        </form>
+                    </CardContent>
+                    <CardFooter class="flex justify-between px-6 pb-6">
+                        <Button variant="outline"> Cancel </Button>
+                        <Button>Deploy</Button>
+                    </CardFooter>
+                </Card>
             </div>
         </div>
     </AppLayout>
